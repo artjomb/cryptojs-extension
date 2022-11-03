d561e736d29285f41c1781c5ee1c8852d53f2b17
Piotr Kaminski <piotr@ideanest.com>
2022-11-03 11:50:22 -0700

    Upgrade dependencies.

--- grunt/tasks/modularize.js
+++ grunt/tasks/modularize.js
@@ -4,7 +4,7 @@ var Fmd = require('fmd');
 
 module.exports = function(grunt) {
 
-  grunt.registerMultiTask('modularize', function () {
+  grunt.registerMultiTask('modularize', function() {
     var options = this.options();
     var done = this.async();
     var modules = {};
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
     };
 
     // Prepare Factory-Module-Definition settings
-    _.each(options.modules, function(conf, name) {
+    _.each(options.modules, (conf, name) => {
       var sources = [];
       var opts = {depends: {'crypto-js/core': 'C'}};
       var deps = [];
@@ -29,23 +29,21 @@ module.exports = function(grunt) {
         do {
           deps = newDeps;
           newDeps = _.chain(deps)
-            .map(function(depName) {return options.modules[depName];})
+            .map(depName => options.modules[depName])
             .flatten()
             .concat(deps)
-            .unique()
+            .uniq()
             .without(name)
-            .select(function(depName) {return options.modules[depName];})
+            .filter(depName => options.modules[depName])
             .sortBy(_.identity)
-            .sort(function(a, b) {
-              return options.modules[a].indexOf(b) >= 0 ? 1 : -1;
-            })
+            .sort((a, b) => options.modules[a].indexOf(b) >= 0 ? 1 : -1)
             .value();
         } while (!_.isEqual(deps, newDeps));
         console.log(name, deps);
 
         // Add components as source files -> results a single file
-        _.each(this.filesSrc, function(source) {
-          _.each(deps, function(depName) {
+        _.each(this.filesSrc, source => {
+          _.each(deps, depName => {
             if (grunt.file.exists(source + depName + '.js')) {
               sources.push(source + depName + '.js');
             }
@@ -55,26 +53,26 @@ module.exports = function(grunt) {
 
         // Add any components that aren't modules as external dependencies instead
         _.chain(conf)
-          .reject(function(depName) {return options.modules[depName];})
-          .map(function(depName) {opts.depends[depName] = null;})
+          .reject(depName => options.modules[depName])
+          .map(depName => {opts.depends[depName] = null;})
           .commit();
 
       } else {
 
         // Find and add self as source
-        _.each(this.filesSrc, function(source) {
+        _.each(this.filesSrc, source => {
           if (grunt.file.exists(source + name + '.js')) sources.push(source + name + '.js');
         }, this);
 
         // Read components and add them as dependecies
-        _.each(_.without(conf, name), function(depName) {
+        _.each(_.without(conf, name), depName => {
           opts.depends[(options.modules[depName] ? './' : '') + depName] = null;
         });
 
       }
 
       // Remove duplicates
-      sources = _.unique(sources);
+      sources = _.uniq(sources);
 
       // Add module settings to fmd definition
       modules[name] = [sources, opts];
@@ -83,20 +81,20 @@ module.exports = function(grunt) {
 
     // Build packege modules
     var fmd = Fmd(_.defaults({factories: options.factories}, config));
-    fs.readdir('node_modules/crypto-js', function(error, files) {
+    fs.readdir('node_modules/crypto-js', (error, files) => {
       if (error) {
         grunt.log.writeln('Failed to scan contents of node_modules/crypto-js: ' + error);
         done(false);
         return;
       }
-      _.each(files, function(filename) {
+      _.each(files, filename => {
         if (/\.js$/.test(filename) && filename !== 'index.js') {
           fmd.vendor(
             'crypto-js/' + filename.replace(/\.js$/, ''),
             filename === 'core.js' ? 'CryptoJS' : null);
         }
       });
-      fmd.define(modules).build(function(createdFiles) {done();});
+      fmd.define(modules).build(createdFiles => {done();});
     });
 
   });
